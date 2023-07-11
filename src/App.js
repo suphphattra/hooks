bd29515cb20e7260fec247eb00b67d3e6c79210c
suphphattra <suphphattra.sr.65@ubu.ac.th>
2023-07-11 12:04:05 +0700

    update react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] =useState(0);
+  const [product1, setProduct1] = useState(150);
+  const [product2, setProduct2] = useState(800);
+  const [product3, setProduct3] = useState(300);
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h3>จำนวนสินค้าในตะกร้า {totalItems}</h3>
+        <h3>ราคาสินค้าในตะกร้าทั้งหมด {totalPrice}</h3>
+      </div>
+      <div>
+        <img src="case.jpg" alt="case" width="15%" /><br />
+        เคสโทรศัพท์ ราคา {product1} บาท
+      </div>
+      <button onClick={()=> {
+        setTotalItems(totalItems + 1);
+        setTotalPrice(totalPrice + product1);
+        }}
         >
-          Learn React
-        </a>
-      </header>
+          เพิ่มเข้าตะกร้า
+        </button>
+          <button 
+          onClick={()=> {
+            if (totalItems > 0 ) {
+            setTotalItems(totalItems - 1);
+            setTotalPrice(totalPrice - product1);
+            }
+          }}
+      >
+        นำออกตะกร้า
+        </button>
+      <div>
+        <img src="cap.jpg" alt="cap" width="15%" /><br />
+        หมวก ราคา {product2} บาท
+      </div>
+      <button onClick={()=> {
+        setTotalItems(totalItems + 1);
+        setTotalPrice(totalPrice + product2);
+        }}
+        >
+          เพิ่มเข้าตะกร้า
+        </button>
+          <button 
+          onClick={()=> {
+            if (totalItems > 0 ) {
+            setTotalItems(totalItems - 1);
+            setTotalPrice(totalPrice - product2);
+            }
+          }}
+      >
+        นำออกตะกร้า
+        </button>
+      <div>
+        <img src="tshirt.jpg" alt="T-Shirt" width="15%" /><br />
+        เสื้อ ราคา {product3} บาท
+      </div>
+      <button onClick={()=> {
+        setTotalItems(totalItems + 1);
+        setTotalPrice(totalPrice + product3);
+        }}
+        >
+          เพิ่มเข้าตะกร้า
+        </button>
+          <button 
+          onClick={()=> {
+            if (totalItems > 0 ) {
+            setTotalItems(totalItems - 1);
+            setTotalPrice(totalPrice - product3);
+            }
+          }}
+      >
+        นำออกตะกร้า
+        </button>
     </div>
   );
 }
